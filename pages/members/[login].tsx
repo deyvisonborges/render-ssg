edf59de93eb6f5054b377a910108e3f5f3c8f426
Deyvison <deyvisonsilvaborges@gmail.com>
2021-12-06 18:01:03 -0300

    add error layer

---
+++ pages/members/[login].tsx
@@ -0,0 +1,68 @@
+import axios from "axios"
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from "next"
+import Image from "next/image"
+import { useRouter } from "next/router"
+
+export default function Member({ user, error, errorMessage }: any) {
+  const { query, isFallback } = useRouter()
+
+
+  console.log(user, error, errorMessage, "sodkfwekoprkweopk")
+
+  if (errorMessage) return <p>{errorMessage}</p>
+
+  if ([401, 403, 404, 500].includes(error)) {
+    return <p>nao foi possivel gerar essa pagina</p>
+  }
+  if (isFallback) return <p>Carregando...</p>
+
+  return (
+    <>
+      <h1>{query.login}</h1>
+      <p>{user.name}</p>
+      <p>{user.bio}</p>
+    </>
+  )
+}
+
+type MemberProps = {
+  login: string
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  try {
+    const { data } = await axios.get(`https://api.github.com/orgs/rocketseat/members`)
+    const paths = data.map((member: MemberProps) => ({ params: { login: member.login.toString() } }))
+
+    return {
+      paths,
+      fallback: true,
+    }
+  }
+  catch {
+    return { paths: [], fallback: false };
+  }
+}
+
+export const getStaticProps: GetStaticProps = async (context: GetStaticPropsContext) => {
+  try {
+    const login = context.params?.login
+    if (!login) return { props: { errorMessage: "algo deu errado" } }
+
+    const { data } = await axios.get(`https://api.github.com/users/${login}`)
+
+    return {
+      props: {
+        user: data?.data || {} as MemberProps
+      }
+    }
+  }
+  catch (error: any) {
+    return {
+      props: {
+        error: error?.response?.status || ""
+      },
+      notFound: false
+    }
+  }
+}
